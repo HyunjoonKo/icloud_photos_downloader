bdb8e91c7f7b5379356ef5c81cc8af3c8ece13f4
Linus <kohyunjoon@icloud.com>
2026-03-02 01:37:29 +0900

    --auto-delete option also applies to the folder structure download feature.

--- src/icloudpd/autodelete.py
+++ src/icloudpd/autodelete.py
@@ -47,7 +47,24 @@ def autodelete_photos(
     """
     logger.info("Deleting any files found in 'Recently Deleted'...")
 
+    from foundation.core import compose
+    from foundation.string_utils import eq, lower
+
+    is_none_folder = compose(eq("none"), lower)
+    is_album_folder = compose(eq("album"), lower)
+    is_album_mode = is_album_folder(folder_structure)
+
     recently_deleted = library_object.recently_deleted
+
+    # In album mode, build a lookup of filenames → full paths by walking the directory once.
+    # This avoids repeated os.walk calls per media item.
+    if is_album_mode:
+        filename_to_paths: dict[str, Set[str]] = {}
+        for root, _, files in os.walk(directory):
+            for file in files:
+                filename_to_paths.setdefault(file, set()).add(os.path.join(root, file))
+
+    delete_local = delete_file_dry_run if dry_run else delete_file
 
     for media in recently_deleted:
         try:
@@ -56,32 +73,13 @@ def autodelete_photos(
             logger.error("Could not convert media created date to local timezone %s", media.created)
             created_date = media.created
 
-        from foundation.core import compose
-        from foundation.string_utils import eq, lower
-
-        is_none_folder = compose(eq("none"), lower)
-
-        if is_none_folder(folder_structure):
-            date_path = ""
-        else:
-            try:
-                date_path = folder_structure.format(created_date)
-            except ValueError:  # pragma: no cover
-                # This error only seems to happen in Python 2
-                logger.error("Photo created date was not valid (%s)", created_date)
-                # e.g. ValueError: year=5 is before 1900
-                # (https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/122)
-                # Just use the Unix epoch
-                created_date = datetime.datetime.fromtimestamp(0)
-                date_path = folder_structure.format(created_date)
-
-        download_dir = os.path.join(directory, date_path)
-
-        paths: Set[str] = set({})
         _size: VersionSize
         versions, filename_overrides = disambiguate_filenames(
             media.versions_with_raw_policy(raw_policy), _sizes, media, lp_filename_generator
         )
+
+        # Collect expected filenames for this media item
+        filenames: Set[str] = set()
         for _size, _version in versions.items():
             if _size in [AssetVersionSize.ALTERNATIVE, AssetVersionSize.ADJUSTED]:
                 version_filename = calculate_version_filename(
@@ -92,10 +90,8 @@ def autodelete_photos(
                     media.item_type,
                     filename_overrides.get(_size),
                 )
-                paths.add(os.path.normpath(local_download_path(version_filename, download_dir)))
-                paths.add(
-                    os.path.normpath(local_download_path(version_filename, download_dir)) + ".xmp"
-                )
+                filenames.add(version_filename)
+                filenames.add(version_filename + ".xmp")
         for _size, _version in media.versions_with_raw_policy(raw_policy).items():
             if _size not in [AssetVersionSize.ALTERNATIVE, AssetVersionSize.ADJUSTED]:
                 version_filename = calculate_version_filename(
@@ -105,12 +101,35 @@ def autodelete_photos(
                     lp_filename_generator,
                     media.item_type,
                 )
-                paths.add(os.path.normpath(local_download_path(version_filename, download_dir)))
-                paths.add(
-                    os.path.normpath(local_download_path(version_filename, download_dir)) + ".xmp"
-                )
-        for path in paths:
-            if os.path.exists(path):
-                logger.debug("Deleting %s...", path)
-                delete_local = delete_file_dry_run if dry_run else delete_file
-                delete_local(logger, path)
+                filenames.add(version_filename)
+                filenames.add(version_filename + ".xmp")
+
+        if is_album_mode:
+            # Search the pre-built filename index for matching files anywhere in the tree
+            for filename in filenames:
+                for path in filename_to_paths.get(filename, set()):
+                    logger.debug("Deleting %s...", path)
+                    delete_local(logger, path)
+        else:
+            if is_none_folder(folder_structure):
+                date_path = ""
+            else:
+                try:
+                    date_path = folder_structure.format(created_date)
+                except ValueError:  # pragma: no cover
+                    # This error only seems to happen in Python 2
+                    logger.error("Photo created date was not valid (%s)", created_date)
+                    # e.g. ValueError: year=5 is before 1900
+                    # (https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/122)
+                    # Just use the Unix epoch
+                    created_date = datetime.datetime.fromtimestamp(0)
+                    date_path = folder_structure.format(created_date)
+
+            download_dir = os.path.join(directory, date_path)
+            paths: Set[str] = set()
+            for filename in filenames:
+                paths.add(os.path.normpath(local_download_path(filename, download_dir)))
+            for path in paths:
+                if os.path.exists(path):
+                    logger.debug("Deleting %s...", path)
+                    delete_local(logger, path)
